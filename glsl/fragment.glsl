7ec4644d4e6e8a43b474859de112114cccffa194
Shu Matsumoto <shumatsumoto0328@gmail.com>
2025-08-25 15:29:34 +0900

    GLSL Snacks 3

--- glsl/fragment.glsl
+++ glsl/fragment.glsl
@@ -4,12 +4,10 @@ uniform float u_time;
 
 void main() {
     vec2 st = gl_FragCoord.xy / u_resolution.xy;
-    
-    // 時間によって変化するグラデーション
-    vec3 color = vec3(0.0);
-    color.r = sin(u_time * 2.0 + st.x * 3.14159) * 0.5 + 0.5;
-    color.g = sin(u_time * 1.5 + st.y * 3.14159) * 0.5 + 0.5;
-    color.b = sin(u_time + (st.x + st.y) * 3.14159) * 0.5 + 0.5;
-    
-    gl_FragColor = vec4(color, 1.0);
+
+    // float d1 = step(0.2, distance(vec2(0.5), st));
+    float d2 = step(0.6, 1.0 - distance(vec2(0.5), st));
+    float color =  d2;
+
+    gl_FragColor = vec4(vec3(color), 1.0);
 }
