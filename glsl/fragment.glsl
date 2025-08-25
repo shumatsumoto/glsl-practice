aa8e5fcea45c80eb2a3c816025c428a1af1c3cca
Shu Matsumoto <shumatsumoto0328@gmail.com>
2025-08-25 15:42:46 +0900

    GLSL Snacks 3

--- glsl/fragment.glsl
+++ glsl/fragment.glsl
@@ -1,13 +1,15 @@
 precision mediump float;
 uniform vec2 u_resolution;
+uniform vec2 u_mouse;
 uniform float u_time;
 
 void main() {
     vec2 st = gl_FragCoord.xy / u_resolution.xy;
+    vec2 mousePos = u_mouse / u_resolution;
+    float distance = distance(vec2(0.5), st);
 
-    // float d1 = step(0.2, distance(vec2(0.5), st));
-    float d2 = step(0.6, 1.0 - distance(vec2(0.5), st));
-    float color =  d2;
+    float color = fract(distance * 10.0 - u_time) / distance / 40.0;
+    vec3 color2 = vec3(.1, .5, 1.0);
 
-    gl_FragColor = vec4(vec3(color), 1.0);
+    gl_FragColor = vec4(color2 * vec3(color), 1.0);
 }
